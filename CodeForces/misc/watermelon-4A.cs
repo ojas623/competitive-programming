9019c8ce2249a63217b8ad342a43c34d17bb7a14
ojas623 <83601895+ojas623@users.noreply.github.com>
2022-01-04 15:44:09 -0800

    build error or sum but code works

---
+++ CodeForces/misc/watermelon-4A.cs
@@ -0,0 +1,17 @@
+using System;
+namespace watermelon{
+class watermelon{
+    public static void Main(string[] args){
+        int w = int.Parse(Console.ReadLine());
+        if(w%2==0){
+            Console.WriteLine("YES");
+        }
+        else if(w == 2){
+            Console.WriteLine("NO");
+        }
+        else{
+            Console.WriteLine("NO");
+        }
+    }
+}
+}
